995055cc2265da6573aea525f69c6204d186b3d5
ailtonbsj <ailton.ifce@gmail.com>
2022-06-28 00:20:27 -0300

    copying template form to data form

--- loiane-angular/forms/src/app/data-form/data-form.component.ts
+++ loiane-angular/forms/src/app/data-form/data-form.component.ts
@@ -20,7 +20,16 @@ export class DataFormComponent implements OnInit {
 
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3)]],
-      email: [null, [Validators.required, Validators.email]]
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null, Validators.required],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required],
+      })
     });
   }
 
